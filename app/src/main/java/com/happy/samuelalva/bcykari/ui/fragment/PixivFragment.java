11cf9f258351eea37fec89149e23192f69bea9d5
SamuelGjk <samuel.alva@outlook.com>
2015-06-16 22:41:18 +0800

    add date select(incomplete)

--- app/src/main/java/com/happy/samuelalva/bcykari/ui/fragment/PixivFragment.java
+++ app/src/main/java/com/happy/samuelalva/bcykari/ui/fragment/PixivFragment.java
@@ -1,16 +1,90 @@
 package com.happy.samuelalva.bcykari.ui.fragment;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.happy.samuelalva.bcykari.R;
 import com.happy.samuelalva.bcykari.support.adapter.PixivPagerAdapter;
 import com.happy.samuelalva.bcykari.support.adapter.base.BasePagerAdapter;
 import com.happy.samuelalva.bcykari.ui.fragment.base.ParentBaseFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Samuel.Alva on 2015/5/6.
  */
 public class PixivFragment extends ParentBaseFragment {
+    private Menu menu;
+    private SimpleDateFormat sdf;
+    private Calendar curCalendar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sdf = new SimpleDateFormat("yyyyMMdd");
+        initCalendar();
+        setHasOptionsMenu(true);
+    }
 
     @Override
     protected BasePagerAdapter getAdapter() {
         return new PixivPagerAdapter(getChildFragmentManager());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_pixiv, menu);
+        this.menu = menu;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String date = null;
+
+        switch (item.getItemId()) {
+            case R.id.menu_the_day_after:
+                date = getAfterDay();
+                break;
+            case R.id.menu_the_day_before:
+                date = getBeforeDay();
+                break;
+        }
+
+        Fragment f = mAdapter.getItemAt(mPager.getCurrentItem());
+        if (f instanceof PixivNormalFragment)
+            ((PixivNormalFragment) f).dateChange(date);
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (menu != null)
+            menu.setGroupVisible(R.id.menu_date_selector, hidden);
+    }
+
+    private void initCalendar() {
+        Date now = new Date();
+        curCalendar = Calendar.getInstance();
+        curCalendar.setTime(now);
+        curCalendar.add(Calendar.DATE, -1);
+    }
+
+    private String getBeforeDay() {
+        curCalendar.add(Calendar.DATE, -1);
+        return sdf.format(curCalendar.getTime());
+    }
+
+    private String getAfterDay() {
+        curCalendar.add(Calendar.DATE, 1);
+        return sdf.format(curCalendar.getTime());
     }
 }
--- app/src/main/java/com/happy/samuelalva/bcykari/ui/fragment/PixivNormalFragment.java
+++ app/src/main/java/com/happy/samuelalva/bcykari/ui/fragment/PixivNormalFragment.java
@@ -1,6 +1,7 @@
 package com.happy.samuelalva.bcykari.ui.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.happy.samuelalva.bcykari.model.StatusModel;
@@ -23,7 +24,6 @@ import java.util.List;
  */
 public class PixivNormalFragment extends ChildBaseFragment {
 
-
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         requestUrl = Constants.DAILY_ILLUST_RANKING_PIXIV;
@@ -35,18 +35,18 @@ public class PixivNormalFragment extends ChildBaseFragment {
     protected List<StatusModel> responseDeal(String response) {
         Document doc = Jsoup.parse(response);
         Elements detailUrls = doc.select("div.ranking-image-item > a");
-        Elements coverList = doc.select("div._layout-thumbnail > img");
-        Elements authorList = doc.getElementsByAttributeValue("class", "icon-text");
-        Elements avatarList = doc.getElementsByAttributeValue("class", "user-icon ui-scroll-view");
+        Elements covers = doc.select("div._layout-thumbnail > img");
+        Elements authors = doc.getElementsByAttributeValue("class", "icon-text");
+        Elements avatars = doc.getElementsByAttributeValue("class", "user-icon ui-scroll-view");
 
         List<StatusModel> data = new ArrayList<>();
         for (int i = 0; i < detailUrls.size(); i++) {
             StatusModel model = new StatusModel();
 
             model.detail = detailUrls.get(i).attr("href");
-            model.cover = coverList.get(i).attr("data-src");
-            model.author = authorList.get(i).html();
-            model.avatar = avatarList.get(i).attr("data-src");
+            model.cover = covers.get(i).attr("data-src");
+            model.author = authors.get(i).html();
+            model.avatar = avatars.get(i).attr("data-src");
 
             data.add(model);
         }
@@ -61,5 +61,11 @@ public class PixivNormalFragment extends ChildBaseFragment {
     @Override
     protected void doRequest(String url, AsyncHttpResponseHandler handler) {
         PixivHttpClient.get(url, handler);
+    }
+
+    public void dateChange(String date) {
+        requestUrl = requestUrl.replace("&p=", "&date=" + date + "&p=");
+        Log.i("requestUrl", requestUrl);
+        doRefresh();
     }
 }
